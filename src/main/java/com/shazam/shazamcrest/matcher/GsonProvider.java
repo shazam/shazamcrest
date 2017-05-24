2ceda78ed3df51c8574b9df66681c4e259f0a68f
Roloreaper <roloreaper@users.noreply.github.com>
2017-05-24 16:22:53 +0200

    More Compilation Changes
    Added Compiler Changes

--- src/main/java/com/shazam/shazamcrest/matcher/GsonProvider.java
+++ src/main/java/com/shazam/shazamcrest/matcher/GsonProvider.java
@@ -18,6 +18,7 @@ import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
